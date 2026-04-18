e31b0787b532fa539e6a734b6a89c8e677cf702d
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-04-18 11:47:29 +0700

    fix UI

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 {
     public static class BuildInfo
     {
-        public const string BUILD_NUMBER = "2026-04-18 / 11:45:47";
+        public const string BUILD_NUMBER = "2026-04-18 / 11:47:19";
     }
 }

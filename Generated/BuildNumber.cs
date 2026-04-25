c122b64af78e971782ec45703149b56cc3b6827c
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-04-25 16:06:29 +0700

    [UI] Add sparse tab height for partition driver browser

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-04-25 / 15:55:23"; 
+        public const string BUILD_NUMBER = "2026-04-25 / 16:06:05"; 
     } 
 }
